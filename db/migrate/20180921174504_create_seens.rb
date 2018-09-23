664aa80bb16d56778dd9167d125f2d6dc6612f6b
SSolanoB <s.solano586@uniandes.co>
2018-09-22 21:44:54 -0500

    try

--- db/migrate/20180921174504_create_seens.rb
+++ db/migrate/20180921174504_create_seens.rb
@@ -2,8 +2,8 @@ class CreateSeens < ActiveRecord::Migration[5.2]
   def change
     create_table :seens do |t|
       t.belongs_to :user
-      t.integer :userid
-      t.integer :postid
+      #t.integer :userid
+      #t.integer :postid
 
       t.timestamps
     end
--- db/migrate/20180921194350_remove_columns_seen.rb
+++ db/migrate/20180921194350_remove_columns_seen.rb
@@ -1,6 +1,6 @@
 class RemoveColumnsSeen < ActiveRecord::Migration[5.2]
   def change
-    remove_column :seens, :userid, :integer
-    remove_column :seens, :postid, :integer
+    #remove_column :seens, :userid, :integer
+    #remove_column :seens, :postid, :integer
   end
 end
